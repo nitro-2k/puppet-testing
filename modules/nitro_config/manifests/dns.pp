68e938c38c8cc48cea1ce78e0be1fdc3c1e36e88
Andrew Petrie <anpetrie@au10147.melb.nitroworld.net>
2018-09-17 17:48:25 +1000

    DNS module first pass

--- modules/nitro_config/manifests/dns.pp
+++ modules/nitro_config/manifests/dns.pp
@@ -1,73 +1,45 @@
-# Class: windows_ad
-#
-# Full description of windows_ad::install here.
-#
-# This class allow you to install/uninstall a windows domain services roles ADDS
-#
-# When you use this class please use it with windows_ad directly. see the readme file.
+# Class: nitro_config::dns
 #
 # === Parameters
 #
 #
 # === Examples
 #
-#  class {'windows_ad::install':
-#  install                => present,
-#  installmanagementtools => true,
-#  installsubfeatures     => true,
-#  restart                => true,
+#  class {'nitro_config::dns':
+#  ensure              => present,
+#  dns1                   => 172.17.10.201,
+#  dns2                   => 172.17.10.203,
 #
 # === Authors
 #
-# Jerome RIVIERE (www.jerome-riviere.re)
+# Andrew Petrie
 #
 # === Copyright
 #
-# Copyright 2014 Jerome RIVIERE.
+# Copyright 2018 Andrew Petrie.
 #
-class windows_ad::install (
+class nitro_config::dns (
     $ensure = $ensure,
-    $installmanagementtools = $installmanagementtools,
-    $installsubfeatures = $installsubfeatures,
-    $restart = $restart,
-    $installflag = $installflag,
+    $dnsservers = $dnsservers,
+    $interface = $::interfaces,
 ) {
-
   validate_re($ensure, '^(present|absent)$', 'valid values for ensure are \'present\' or \'absent\'')
-  validate_bool($installmanagementtools)
-  validate_bool($installsubfeatures)
-  validate_bool($restart)
-  validate_bool($installflag)
+  #validate_ip_address($dns1)
+  #validate_ip_address($dns2)
+  #validate_bool($installflag)
 
-  if ($installflag == true){
-    if $::operatingsystem != 'windows' { fail ("${module_name} not supported on ${::operatingsystem}") }
-    if $restart { $restartbool = 'true' } else { $restartbool = 'false' }
-    if $installsubfeatures { $subfeatures = '-IncludeAllSubFeature' }
+  if ($::operatingsystem = 'windows'){
+    # Windows 2012 and newer required http://technet.microsoft.com/en-us/library/ee662309.aspx
+    if $::kernelversion !~ /^(6\.2|6\.3|10\.0)/ { fail ("${module_name} requires Windows 2012 or newer") }
 
-    if $::kernelversion =~ /^(6\.1)/ and $installmanagementtools {
-      fail ('Windows 2012 or newer is required to use the installmanagementtools parameter')
-    } elsif $installmanagementtools {
-      $managementtools = '-IncludeManagementTools'
-    }
-
-    # Windows 2008 R2 and newer required http://technet.microsoft.com/en-us/library/ee662309.aspx
-    if $::kernelversion !~ /^(6\.1|6\.2|6\.3|10\.0)/ { fail ("${module_name} requires Windows 2008 R2 or newer") }
-
-    # from Windows 2012 'Add-WindowsFeature' has been replaced with 'Install-WindowsFeature' http://technet.microsoft.com/en-us/library/ee662309.aspx
     if ($ensure == 'present') {
-      if $::kernelversion =~ /^(6.1)/ { $command = 'Add-WindowsFeature' } else { $command = 'Install-WindowsFeature' }
-
-      exec { "add-feature-${title}":
-        command   => "Import-Module ServerManager; ${command} AD-Domain-Services ${managementtools} ${subfeatures} -Restart:$${restartbool}",
-        onlyif    => "Import-Module ServerManager; if (@(Get-WindowsFeature AD-Domain-Services | ?{\$_.Installed -match \'false\'}).count -eq 0) { exit 1 }",
-        provider  => powershell,
-      }
-    } elsif ($ensure == 'absent') {
-      exec { "remove-feature-${title}":
-        command   => "Import-Module ServerManager; Remove-WindowsFeature AD-Domain-Services -Restart:$${restartbool}",
-        onlyif    => "Import-Module ServerManager; if (@(Get-WindowsFeature AD-Domain-Services | ?{\$_.Installed -match \'true\'}).count -eq 0) { exit 1 }",
+      exec { "Update DNS":
+        command   => template("nitro_config/dns.ps1.erb"),
         provider  => powershell,
       }
     }
-  }
+  } elsif ($::operatingsystem = 'CentOS') {
+    #Write some Linux DNS stuff :)
+
+  } else { fail ("${module_name} not supported on ${::operatingsystem}") }
 }
